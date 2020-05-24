e189d91f14dc97c35ca96e30c0121822b154ba0b
Денис <zololotarenko.2015@gmail.com>
2020-05-24 18:05:53 +0300

    add helmet protection

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,24 +1,28 @@
 /* external modules */
 import express from 'express';
 import cors from 'cors';
-import path from 'path'
+import * as bodyParser from 'body-parser';
+import path from 'path';
 /* other */
-import { expressLogger } from "../logger";
+import helmetProtection from './helmet-protection';
+import { expressLogger } from '../logger';
 
 const app: express.Application = express();
 
-app.set('port', (process.env.PORT || 3001))
-app.set('trust proxy', ['loopback', 'uniquelocal'])
+app.set('port', process.env.PORT || 3002);
+app.set('trust proxy', ['loopback', 'uniquelocal']);
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.use('/public', express.static(path.join(__dirname, 'public'), { maxAge: '7d' }));
 app.use(expressLogger);
 
+helmetProtection(app);
+
 app.get('/', (req, res) => {
-    console.log('req.body => ', req.body)
-    res.send({ Y2: 'hello world!!!' })
-})
+  console.log('req.body => ', req.body);
+  res.send({ Y2: 'hello world!!!' });
+});
 
 export default app;
--- src/logger.ts
+++ src/logger.ts
@@ -1,65 +1,63 @@
 import log4js from 'log4js';
 
-log4js.addLayout('json', config => (logEvent) => {
-    const customEvent = {
-        startTime: logEvent.startTime,
-        level: logEvent.level.levelStr,
-        data: logEvent.data
-    }
-    return JSON.stringify(customEvent) + config.separator
+const isDev = process.env.NODE_ENV === 'development';
+
+log4js.addLayout('json', (config) => (logEvent) => {
+  const customEvent = {
+    startTime: logEvent.startTime,
+    level: logEvent.level.levelStr,
+    data: logEvent.data,
+  };
+  return JSON.stringify(customEvent) + config.separator;
 });
 
 log4js.configure({
-    appenders: {
-        console: {
-            type: 'console',
-            layout: {
-                type: 'pattern',
-                pattern: '[%[%p%]] %r %[=>%] %m'
-            }
-        },
-        'text-file': {
-            type: 'file',
-            filename: 'text.log'
-        },
-        'json-file': {
-            type: 'file',
-            filename: 'app.log',
-            layout: {
-                type: 'json',
-                separator: ','
-            }
-        }
+  appenders: {
+    console: {
+      type: 'console',
+      layout: {
+        type: 'pattern',
+        pattern: '[%[%p%]] %r %[=>%] %m',
+      },
     },
-    categories: {
-        'text-file': {
-            appenders: ['text-file'],
-            level: 'all'
-        },
-        'json-file': {
-            appenders: ['json-file'],
-            level: 'all'
-        },
-        default: {
-            appenders: ['console'],
-            level: 'all'
-        }
-    }
-})
+    'text-file': {
+      type: 'file',
+      filename: 'text.log',
+    },
+    'json-file': {
+      type: 'file',
+      filename: 'app.log',
+      layout: {
+        type: 'json',
+        separator: ',',
+      },
+    },
+  },
+  categories: {
+    'text-file': {
+      appenders: ['text-file'],
+      level: 'all',
+    },
+    'json-file': {
+      appenders: ['json-file'],
+      level: isDev ? 'all' : 'error',
+    },
+    default: {
+      appenders: ['console'],
+      level: 'all', //TODO
+    },
+  },
+});
 
 const logger = log4js.getLogger();
 
-const textLogger = log4js.getLogger('text-file')
-const jsonLogger = log4js.getLogger('json-file')
+const textLogger = log4js.getLogger('text-file');
+const jsonLogger = log4js.getLogger('json-file');
 
 const expressLogger = log4js.connectLogger(logger, {
-    level: 'info',
-    format: (req, res, format) => format(`":method :url" :status ${JSON.stringify(req.body) || ''}`)
-})
+  level: 'info',
+  format: (req, res, format) => format(`":method :url" :status ${JSON.stringify(req.body) || ''}`),
+});
 
 export default logger;
-export {
-    jsonLogger as fileLogger,
-    textLogger,
-    expressLogger
-}
+export { jsonLogger as fileLogger, textLogger, expressLogger };
